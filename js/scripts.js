3388e205780e4d649972411a73ed56289a56f637
Bronislavson <alexnavroc@gmail.com>
2022-09-12 19:37:33 +0300

    posmotry tak dopustimo? dostavka i kartochki

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,24 @@
 		burger.classList.toggle('is-active');
 	});
 }());
+
+function openPost(evt, postName) {
+	// Объявить все переменные
+	let i, tabcontent, tablinks;
+ 
+	// Получить все элементы с помощью class="tabcontent" и спрятать их
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	tabcontent[i].style.display = "none";
+	}
+
+	// Получить все элементы с помощью class="tablinks" и удалить class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	tablinks[i].className = tablinks[i].className.replace(" has-active", "");
+	}
+
+	// Показать текущую вкладку и добавить "active" класс для кнопки, которая открыла вкладку
+	document.getElementById(postName).style.display = "flex";
+	evt.currentTarget.className += " has-active";
+ };
